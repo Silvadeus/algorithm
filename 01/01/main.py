f14962ad5ce93b508b18dc8355c8b5f637c9933e
Tornike <tmamatsashvili@productsavvy.com>
2018-09-30 21:45:37 +0400

    Initial commit

---
+++ 01/01/main.py
@@ -0,0 +1,19 @@
+'''
+    T.M
+    Homework #1.1
+'''
+
+    
+def multiple_finder(a, b, c):
+    if (a > b):
+        more, less = a, b
+    else:
+        more, less = b, a
+
+    return (more-1) // c - less // c
+
+a = int(input("a - "))
+b = int(input("b - "))
+c = int(input("c - "))
+
+print("multiples {}".format(multiple_finder(a, b, c)))
